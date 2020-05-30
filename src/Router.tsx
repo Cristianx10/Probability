a05c60d30b659f3550ee8617e73c2d07fea59ede
Cristianx10 <42477569+Cristianx10@users.noreply.github.com>
2020-05-30 18:55:49 -0500

    configuaacion de rutas

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,25 @@
-import React from "react";
+import loadable from '@loadable/component';
+import React, { lazy, Suspense } from "react";
 import {
     Switch,
     Route,
 } from "react-router";
 import { BrowserRouter } from 'react-router-dom'
-import App from './components/App/App';
+
+const App = loadable(() => import('./components/App/App'));
+const Login = loadable(() => import("./components/Registro/Login/Login"));
+const SplatScreen = loadable(() => import('./components/SplatScreen/SplatScreen'));
 
 const Router = () => {
     return <BrowserRouter>
         <Switch>
-            <Route path="/" ><App /></Route>
+
+            <Route path="/" component={SplatScreen} exact></Route>
+
+            <Route path="/login" component={Login} exact />
+
+            <Route path="/index" exact><App /></Route>
+
         </Switch>
     </BrowserRouter>
 }
